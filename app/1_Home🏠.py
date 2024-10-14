42da7f0855da993c6b24a2294ba4ebe89f4d97c7
Aidil Adham <aidiladham@invokeisdata.com>
2024-10-14 10:19:06 +0800

    feat: Update features

---
+++ app/1_Home🏠.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Iris Flower Prediction App
+         
+This app predicts the **Iris flower** type!
+""")
